1518c4241c54a0b6769c71cb00248537c78ceafa
Patrick-André Holzmann <patrickholzmann4@gmail.com>
2025-06-23 13:20:22 +0200

    Update insertion_sort.py

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -1,1 +1,16 @@
 
+class InsertionSort():
+    def __init__(self, arr):
+        self.arr = arr
+
+    def algorithm(self):
+        n = len(self.arr)
+        for i in range(1, n):
+            key = self.arr[i]
+            j = i - 1
+            while j >= 0 and self.arr[j] > key:
+                self.arr[j + 1] = self.arr[j]
+                j -= 1
+
+            self.arr[j + 1] = key
+        return self.arr
