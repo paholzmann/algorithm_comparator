17718e63d1cc77220292dd3f08a116c3959b2886
Patrick-André Holzmann <patrickholzmann4@gmail.com>
2025-06-23 13:05:40 +0200

    Update bubble_sort.py

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,1 +1,13 @@
 
+class BubbleSort:
+    def __init__(self, arr):
+        self.arr = arr
+
+
+    def algorithm(self):
+        n = len(self.arr)
+        for i in range(n):
+            for j in range(n - 1):
+                if self.arr[j] > self.arr[j + 1]:
+                    self.arr[j], self.arr[j + 1] = self.arr[j + 1], self.arr[j]
+        return self.arr
