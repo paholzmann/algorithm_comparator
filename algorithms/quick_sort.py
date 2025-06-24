190c2461ac9b2cee30a9aa9f6a1e300663ae1670
Patrick-André Holzmann <patrickholzmann4@gmail.com>
2025-06-24 12:58:06 +0200

    Update quick_sort.py

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -1,1 +1,25 @@
 
+
+class QuickSort():
+    def __init__(self, arr):
+        self.arr = arr
+
+    def algorithm(self):
+        n = len(self.arr)
+        if n <= 1:
+            return self.arr
+        pivot = n // 2
+
+        left_list = []
+        middle_list = []
+        right_list = []
+
+        for i in range(n):
+            if self.arr[i] < self.arr[pivot]:
+                left_list.append(self.arr[i])
+            elif self.arr[i] > self.arr[pivot]:
+                right_list.append(self.arr[i])
+            elif self.arr[i] == self.arr[pivot]:
+                middle_list.append(self.arr[i])
+
+        return self.__class__(left_list).algorithm() + middle_list + self.__class__(right_list).algorithm()
