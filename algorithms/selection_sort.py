7b31429855120b97d221cbb1da0ff3fec44b8111
Patrick-André Holzmann <patrickholzmann4@gmail.com>
2025-06-25 11:23:18 +0200

    Update selection_sort.py

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -1,10 +1,30 @@
 
 class SelectionSort():
+    """
+    Class to sort all numeric values in a list using the selection sort algorithm.
+
+    Attributes:
+        arr (list): List of numbers (int or float) to be sorted.
+    """
     def __init__(self, arr):
+        """
+        Initializes the class with a list of numbers.
+
+        Args:
+            arr (list): List of numbers (int or float).
+        """
         self.arr = arr
 
     def algorithm(self):
+        """
+        Sorts all numbers in the list ascending order using selection sort.
+
+        Retruns:
+            list: Sorted list in ascending order
+        """
         n = len(self.arr)
+        if n <= 1:
+            return self.arr
         for i in range(n):
             min_index = i
             for j in range(i + 1, n):
