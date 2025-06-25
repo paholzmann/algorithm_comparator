cbc98b2e06362935fe4f3b1ff1a5962bdc55e4e1
Patrick-André Holzmann <patrickholzmann4@gmail.com>
2025-06-25 11:27:05 +0200

    Update reverse_list.py

--- algorithms/reverse_list.py
+++ algorithms/reverse_list.py
@@ -1,10 +1,30 @@
 
 class ReverseList():
+    """
+    Class to reverse the elements in a list.
+
+    Attributes:
+    arr (list): List of numbers (int or float) to be reversed.
+    """
     def __init__(self, arr):
+        """
+        Initializes the class with a list of numbers.
+
+        Args:
+            arr (list): List of numbers (int or float).
+        """
         self.arr = arr
 
     def algorithm(self):
+        """
+        Reverses the elements in the list in place.
+
+        Returns:
+            list: A list with all elements in reversed order.
+        """
         n = len(self.arr)
+        if n <= 1:
+            return self.arr
         start = 0
         end = n - 1
         while start < end:
